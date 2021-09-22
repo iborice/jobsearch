c2b9d226bbb0ec27d0277b7cd01394d62f71e249
iborice <iborice3@gmail.com>
2021-09-23 00:25:54 +0100

    deploy1

--- mysite/settings.py
+++ mysite/settings.py
@@ -152,7 +152,7 @@ MEDIA_URL = '/media/'
 if os.environ.get('ENV') == 'PRODUCTION':
 
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
+    STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
     # Extra places for collectstatic to find static files.
