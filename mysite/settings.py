99fd27b91884fceb12638d657d2c15bba2a50d90
iborice <iborice3@gmail.com>
2021-09-22 23:25:41 +0100

    resultat du projet avec les fonctionnalites de bases

--- mysite/settings.py
+++ mysite/settings.py
@@ -149,6 +149,7 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
